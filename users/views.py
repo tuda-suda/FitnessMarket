2663fe5408b3193e0cdd04d76de3bb642eb2dbd9
tuda-suda <marat.gumerov99@gmail.com>
2020-07-08 22:22:11 +0500

    🔗Merge views_profiles

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Client, Trainer
 
-# Create your views here.
+
+def profile_client(request, username):
+    client = get_object_or_404(Client, username=username)
+    return render(request, "client_profile.html", {"client": client})
+
+
+def profile_trainer(request, username):
+    trainer = get_object_or_404(Trainer, username=username)
+    return render(request, "trainer_profile.html", {"trainer": trainer})
